dfbec6f48bfb43016d8b7dee7f29c81ffed74894
Guillermoscript <guillomarindavil@gmail.com>
2021-04-14 15:14:24 -0400

    adding the Validation conrtoller so i do no repeat myself and its easier to validate fields :D

---
+++ admin/controllers/ValidationPaymentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace base\admin\controllers;
+/**
+ * The core plugin Rest Api class.
+ *
+ * This is used to validate all the data that will enter in the billing inputs.
+ *
+ * @since      1.0.0
+ * @package    Pagos_Offline_Venezuela
+ * @subpackage Pagos_Offline_Venezuela/includes
+ * @author     Guillermo <guillomarindavila@gmail.com>
+ */
+class ValidationPaymentController
+{
+    public static function validate_fields()
+    {
+
+        $array_errores = [];
+
+        if( empty( $_POST[ 'billing_first_name' ]) ) {
+            wc_add_notice(  'El nombre es requerido', 'error' );
+            $array_errores[] = false;
+        }
+
+        if( empty( $_POST[ 'billing_last_name' ]) ) {
+            wc_add_notice(  'El apellido es requerido', 'error' );
+            $array_errores[] = false;
+        }
+
+        if( empty( $_POST[ 'billing_country' ]) ) {
+            wc_add_notice(  'El pais es requerido', 'error' );
+            $array_errores[] = false;
+        }
+
+        if( empty( $_POST[ 'billing_phone' ]) ) {
+            wc_add_notice(  'El celular es requerido', 'error' );
+            $array_errores[] = false;
+        }
+
+        if( !preg_match('/(^(\+58\s?)?(\(\d{3}\)|\d{4})([\s\-]?\d{3})([\s\-]?\d{4})$)/',  $_POST[ 'billing_phone' ] ) ) {
+            // if( !preg_match('/\d/',  $_POST[ 'billing_phone' ] ) && strlen($_POST['billing_phone']) > 7 || strlen($_POST['billing_phone']) < 7 ) {
+            wc_add_notice(  'El Numero no esta en el formato aceptado', 'error' );
+            $array_errores[] = false;
+        }
+
+        if( !preg_match('/^\d+$/',  $_POST[ 'billing_cid' ] ) ) {
+            
+            wc_add_notice(  'La cedula no esta en el formato aceptado', 'error' );
+            $array_errores[] = false;
+        }
+        
+
+        if( empty( $_POST[ 'billing_cid' ]) ) {
+            wc_add_notice(  'La cedula es requerida', 'error' );
+            $array_errores[] = false;
+        }
+
+        if (empty($array_errores)) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+}
--- admin/controllers/pago_movil_payment.php
+++ admin/controllers/pago_movil_payment.php
@@ -1,5 +1,7 @@
 <?php
 
+use base\admin\controllers\ValidationPaymentController;
+
 function wc_offline_gateway_init_pago_movil() {
 
     class WC_Gateway_Offline_Pago_Movil extends WC_Payment_Gateway
@@ -463,52 +465,7 @@ function wc_offline_gateway_init_pago_movil() {
 
         public function validate_fields()
         {
-            $array_errores = [];
- 
-            if( empty( $_POST[ 'billing_first_name' ]) ) {
-                wc_add_notice(  'El nombre es requerido', 'error' );
-                $array_errores[] = false;
-            }
- 
-            if( empty( $_POST[ 'billing_last_name' ]) ) {
-                wc_add_notice(  'El apellido es requerido', 'error' );
-                $array_errores[] = false;
-            }
- 
-            if( empty( $_POST[ 'billing_country' ]) ) {
-                wc_add_notice(  'El pais es requerido', 'error' );
-                $array_errores[] = false;
-            }
- 
-            if( empty( $_POST[ 'billing_phone' ]) ) {
-                wc_add_notice(  'El celular es requerido', 'error' );
-                $array_errores[] = false;
-            }
- 
-            if( !preg_match('/(^(\+58\s?)?(\(\d{3}\)|\d{4})([\s\-]?\d{3})([\s\-]?\d{4})$)/',  $_POST[ 'billing_phone' ] ) ) {
-                
-                wc_add_notice(  'El Numero no esta en el formato aceptado', 'error' );
-                $array_errores[] = false;
-            }
-
-            if( !preg_match('/^\d+$/',  $_POST[ 'billing_cid' ] ) ) {
-                
-                wc_add_notice(  'La cedula no esta en el formato aceptado', 'error' );
-                $array_errores[] = false;
-            }
-            
- 
-            if( empty( $_POST[ 'billing_cid' ]) ) {
-                wc_add_notice(  'La cedula es requerida', 'error' );
-                $array_errores[] = false;
-            }
-
-            if (empty($array_errores)) {
-                return true;
-            } else {
-                return false;
-            }
-
+           ValidationPaymentController::validate_fields();
         }
 
         public function payment_scripts() 
