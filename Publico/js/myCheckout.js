af99193b0fc57813347e6c5e0030f241e46417c8
Guillermoscript <arepayquezo@gmail.com>
2022-03-13 10:56:23 -0400

    clean up and delete Object mutation

--- Publico/js/myCheckout.js
+++ Publico/js/myCheckout.js
@@ -8,10 +8,13 @@ import {
     validationCheckout,
 } from './checkoutAjax.js';
 
-// console.log('AAAAAAAAAAAAAAAAAAAAAA FUCKKKKKKKKKKKKKKKKKKKKKKKKKKKKK');
-
 jQuery(document).ready(() => {
-    // observerWrapper()
+
+    /**
+    * Check if the DOM have been modified by woocommerce (the checkout button) by an ajax call
+    * and if thats true then add events to the checout button.
+    * 
+    */
     jQuery('body').on('updated_checkout', () => {
         addEventsToCheckoutButon()
         addTextToInputFileWhenUserClick()
@@ -24,48 +27,6 @@ function finishCheckout() {
     if (validationContainer(ajax_var)) {
         document.getElementById('place_order').removeEventListener('click',finishCheckout)
     }
-}
-
-
-/**
-* Check if the DOM have been modified by woocommerce (the checkout button) by an ajax call
-* and if thats true then add events to the checout button.
-* 
-*/
-function observerWrapper() {
-    // target element that we will observe
-    const target = document.getElementsByClassName('checkout')[0];
-
-    // config object
-    const config = {
-        characterData: true,
-        childList: true,
-        subtree: true
-    };
-
-    // subscriber function
-    function subscriber(mutations) {
-        mutations.forEach(mutation => {
-            // console.log(mutation);
-            if (mutation.target === document.getElementById('place_order')) {
-                if (mutation.addedNodes.length > 0) {
-                    // console.log('aaa');
-                    addEventsToCheckoutButon()
-                    addTextToInputFileWhenUserClick()
-                }
-            }
-            if (mutation.addedNodes[0] === document.querySelector('.woocommerce-NoticeGroup.woocommerce-NoticeGroup-checkout')) {
-                document.querySelector('.blockUI.blockOverlay').remove()
-            }
-        })
-
-    }
-
-    // instantiating observer
-    const observer = new MutationObserver(subscriber);
-
-    // observing target
-    observer.observe(target, config);
 }
 
 // funcion que tla vez ponga pero por ahora no 
