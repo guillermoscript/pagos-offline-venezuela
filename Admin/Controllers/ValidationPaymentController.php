9a21081377d236f34125124d84b0ecbd13bb73bf
Guillermo <guillermo.marin@roraimadevs.com>
2022-10-24 17:05:06 -0400

    change to named , last one was a mess lol and added json response

--- Admin/Controllers/ValidationPaymentController.php
+++ Admin/Controllers/ValidationPaymentController.php
@@ -66,39 +66,39 @@ class ValidationPaymentController
     {
         # code...
         $array_errors = [];
-        $inputs = ['email_origen','zelle-select','reference_number','zelle_sender_name'];
-        if (!isset($_POST[$inputs[0]]) && !empty($_POST[$inputs[0]])) {
-            wc_add_notice(  '¡Error! El campo del correo esta vacio, por favor ingrese un correo.', 'error' );
-            $array_errors[] = false;
+        $inputs = ['email_origen','zelle_select','reference_number','zelle_sender_name'];
+        if (!isset($_POST['email_origen']) && !empty($_POST['email_origen'])) {
+            
+            $array_errors[] = (  '¡Error! El campo del correo esta vacio, por favor ingrese un correo.' );
         } 
-        if (!isset($_POST[$inputs[1]]) && !empty($_POST[$inputs[1]])) {
-            wc_add_notice(  '¡Error! No selecciono un correo zelle, por favor seleccione uno.', 'error' );
-            $array_errors[] = false;
+        if (!isset($_POST['zelle_select']) && !empty($_POST['zelle_select'])) {
+            
+            $array_errors[] = (  '¡Error! No selecciono un correo zelle, por favor seleccione uno.' );
         } 
 
-        if (!isset($_POST[$inputs[2]]) && !empty($_POST[$inputs[2]])) {
-            wc_add_notice(  '¡Error! No Coloco el numero de referencia, por favor seleccione uno.', 'error' );
-            $array_errors[] = false;
+        if (!isset($_POST['reference_number']) && !empty($_POST['reference_number'])) {
+            
+            $array_errors[] = (  '¡Error! No Coloco el numero de referencia, por favor seleccione uno.' );
         } 
         
-        if (!ctype_alnum($_POST[$inputs[2]])) {
-            wc_add_notice(  '¡Error! El numero de referencia no es valido, por favor ingrese uno valido.', 'error' );
-            $array_errors[] = false;
+        if (!ctype_alnum($_POST['reference_number'])) {
+            
+            $array_errors[] = (  '¡Error! El numero de referencia no es valido, por favor ingrese uno valido.' );
         } 
 
-        if (!isset($_POST[$inputs[3]]) && !empty($_POST[$inputs[3]])) {
-            wc_add_notice(  '¡Error! No Coloco el nombre, por favor seleccione uno.', 'error' );
-            $array_errors[] = false;
+        if (!isset($_POST['zelle_sender_name']) && !empty($_POST['zelle_sender_name'])) {
+            
+            $array_errors[] = (  '¡Error! No Coloco el nombre, por favor seleccione uno.' );
         } 
 
-        if (!preg_match("/^([a-zA-Z' ]+)$/",$_POST[$inputs[3]])) {
-            wc_add_notice(  '¡Error! El nombre de origen no es valido, por favor ingrese uno valido.', 'error' );
-            $array_errors[] = false;
+        if (!preg_match("/^([a-zA-Z' ]+)$/",$_POST['zelle_sender_name'])) {
+            
+            $array_errors[] = (  '¡Error! El nombre de origen no es valido, por favor ingrese uno valido.' );
         }
 
-        if (filter_var($_POST[$inputs[0]],FILTER_VALIDATE_EMAIL) === false) {
-            wc_add_notice(  '¡Error! El correo de origen no es valido, por favor ingrese uno valido.', 'error' );
-            $array_errors[] = false;
+        if (filter_var($_POST['email_origen'],FILTER_VALIDATE_EMAIL) === false) {
+            
+            $array_errors[] = (  '¡Error! El correo de origen no es valido, por favor ingrese uno valido.' );
         }
 
         $count_de_cuenta = 0;
@@ -110,15 +110,15 @@ class ValidationPaymentController
                 $count_de_cuenta++;
             }
             if ($count_de_cuenta === 0) {
-                wc_add_notice('Error el email que selecciono no es uno de los mostrados', 'error' );
-                $array_errors[] = false;
+                
+                $array_errors[] = ('Error el email que selecciono no es uno de los mostrados' );
             }
         }  
 
         if (empty($array_errors)) {
             return true;
         } else {
-            return false;
+            return json_encode($array_errors);
         }
     }
 
@@ -180,22 +180,21 @@ class ValidationPaymentController
     public static function validate_pago_movil()
     {
         $array_errors = [];
-        $inputs = ['telefono_movil'];
-        if (!isset($_POST[$inputs[0]]) && !empty($_POST[$inputs[0]])) {
-            wc_add_notice(  '¡Error! El campo del telefono esta vacio, por favor ingrese un correo.', 'error' );
-            $array_errors[] = false;
+        if (!isset($_POST['telefono_movil']) && !empty($_POST['telefono_movil'])) {
+            
+            $array_errors[] = (  '¡Error! El campo del telefono esta vacio, por favor ingrese un correo.');
         }
 
-        if( !preg_match('/(^(\+58\s?)?(\d{3}|\d{4})([\s\-]?\d{3})([\s\-]?\d{4})$)/',  $_POST[ $inputs[0] ] ) ) {
+        if( !preg_match('/(^(\+58\s?)?(\d{3}|\d{4})([\s\-]?\d{3})([\s\-]?\d{4})$)/',  $_POST[ 'telefono_movil' ] ) ) {
             // if( !preg_match('/\d/',  $_POST[ 'billing_phone' ] ) && strlen($_POST['billing_phone']) > 7 || strlen($_POST['billing_phone']) < 7 ) {
-            wc_add_notice(  'El Número del pago movil no esta en el formato aceptado', 'error' );
-            $array_errors[] = false;
+            
+            $array_errors[] = (  'El Número del pago movil no esta en el formato aceptado');
         }
 
         if (empty($array_errors)) {
             return true;
         } else {
-            return false;
+            return json_encode($array_errors);
         }
     }
 
